dcc6a6c4f806e4b14d6527b74ff9c5a2244b5c5f
China Hoffman <china.hoffman@gmail.com>
2022-04-29 12:09:23 -0400

    using map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ const welcome = {
   title: "React",
   greeting: "Hey",
 };
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
@@ -15,6 +33,21 @@ function App() {
         Search:
         <input id="search" type="text" />
       </label>
+      <h2>Topics</h2>
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span> {item.author}</span>
+              <span> {item.num_comments}</span>
+              <span> {item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
